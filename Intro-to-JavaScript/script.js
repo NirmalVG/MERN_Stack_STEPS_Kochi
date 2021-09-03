db91a9ad71e5783470c4b04f66aee159f5fc9e48
NIRMAL V G <38987580+NirmalVG@users.noreply.github.com>
2021-09-03 02:13:02 -1200

    Commit changes

---
+++ Intro-to-JavaScript/script.js
@@ -0,0 +1,27 @@
+function addNumbers() {
+    var a=30;
+    var b=20;
+    var sum=a+b;
+    document.getElementById('add').innerHTML=sum;
+}
+
+function subtractNumbers() {
+    var a=30;
+    var b=20;
+    var sum=a-b;
+    document.getElementById('subtract').innerHTML=sum;
+}
+
+function multiplyNumbers() {
+    var a=30;
+    var b=20;
+    var sum=a*b;
+    document.getElementById('multiply').innerHTML=sum;
+}
+
+function divideNumbers() {
+    var a=30;
+    var b=20;
+    var sum=a/b;
+    document.getElementById('divide').innerHTML=sum;
+}
